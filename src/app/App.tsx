ca293f49ef492c89993b702f261e386e53a0d4bf
Sergey Molchanov <molchanov.sergei@neuro-city.ru>
2023-09-12 17:44:17 +0300

    change structure and config imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "pages/MainPage/MainPage.async";
 import {Suspense} from "react";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
 
 
 export const App = () => {
