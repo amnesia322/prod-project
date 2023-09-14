1924046c898b915c7b1930060a04bf683245e479
Sergey Molchanov <molchanov.sergei@neuro-city.ru>
2023-09-14 19:22:18 +0300

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,18 @@
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "pages/MainPage/MainPage.async";
-import {Suspense} from "react";
-import {classNames} from "helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 export const App = () => {
-    const {theme, toggleTheme } = useTheme();
-
+    const {theme} = useTheme();
     return <div className={classNames('app', {}, [theme])}>
-        <button onClick={toggleTheme}>TOGGLE</button>
-        <Link to={'/'}>Main</Link>
-        <Link to={'/about'}>About</Link>
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={'/about'} element={<AboutPageAsync/>}/>
-                <Route path={'/'} element={<MainPageAsync/>}/>
-            </Routes>
-        </Suspense>
+        <Navbar/>
+        <div className='content-page'>
+            <Sidebar/>
+            <AppRouter/>
+        </div>
     </div>
 }
